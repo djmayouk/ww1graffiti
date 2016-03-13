6fc01d8d0451b5449e1c21885998b93737272d3d
David Mayo <github@davidmayo.co.uk>
2016-03-13 12:21:09 +0000

    Removing Facebook app secret from published code and changed it

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '990425327702070', '991f16b1036f77871a7504d96574aec5', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
